b48539f67d72131328e9e76f46b91fd879c53a3e
zhangqiang <inteli_zq@163.com>
2021-07-01 21:28:03 +0800

    add common functions

--- dlink-common-hadoop/src/main/scala/com/sensetime/bigdata/hadoop/hdfs/HDFSConfig.scala
+++ dlink-common-hadoop/src/main/scala/com/sensetime/bigdata/hadoop/hdfs/HDFSConfig.scala
@@ -14,7 +14,7 @@ class HDFSConfig extends GenericObjectPoolConfig[FileSystem] {
   // 逐出连接的最小空闲时间
   this.setMinEvictableIdleTimeMillis(60000L)
   // 逐出扫描的时间间隔(毫秒) 如果为负数则不运行逐出线程，默认-1
-  this.setTimeBetweenEvictionRunsMillis(30000L)
+  this.setTimeBetweenEvictionRunsMillis(-1)
   // 每次逐出检查时, 逐出的最大数目
   this.setNumTestsPerEvictionRun(-1)
 
