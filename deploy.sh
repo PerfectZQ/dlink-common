4cdda1d5f61a18e563415fa5061cb067741443d6
zhangqiang <inteli_zq@163.com>
2021-07-05 16:43:10 +0800

    add common functions

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,9 @@
-mvn deploy -s settings.xml -Dmaven.test.skip=true -P java8-compiler-windows
+module="$1"
+
+if [[ "$module" != "" ]]; then
+    echo "Deploy module: ${module}..."
+    mvn clean deploy -s settings.xml -Dmaven.test.skip=true -P java8-compiler-windows -pl $module
+else
+    mvn clean deploy -s settings.xml -Dmaven.test.skip=true -P java8-compiler-windows
+fi
+
